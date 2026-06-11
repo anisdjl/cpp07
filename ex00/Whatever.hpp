8374303093e2bb5a41dbb898aa7219a4d01c1ea9
anisdjl <adjelili954@gmail.com>
2026-06-11 18:54:22 +0200

    ex01 begin

--- ex00/Whatever.hpp
+++ ex00/Whatever.hpp
@@ -11,10 +11,10 @@ template <typename T> T min(T x, T y) {
 	return (x < y) ? x : y;
 }
 
-template<typename T> void swap(T *x, T *y) {
-	T tmp = *x;
-	*x = *y;
-	*y = tmp;
+template<typename T> void swap(T &x, T &y) {
+	T tmp = x;
+	x = y;
+	y = tmp;
 }
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,12 +2,19 @@
 
 int	main(void)
 {
-	int x, y;
+	int a = 2;
+	int b = 3;
 
-	x = 0, y = 1;
-	std::cout << max<int>(7, 9) << std::endl;
-	std::cout << min<float>(5.7, 9.8) << std::endl;
-	swap<int>(&x, &y);
-	std::cout << "x: " << x << " y: " << y << std::endl;
-	return (0);
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
